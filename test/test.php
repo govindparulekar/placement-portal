16ec8cfc0668810193793c10d0b40fe1826f430f
govindparulekar <devgovind511@gmail.com>
2021-06-15 16:02:26 +0530

    'yo'

--- test/test.php
+++ test/test.php
@@ -1,13 +1,2 @@
 <?php
-include_once '../vendor/autoload.php';
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-
-$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('test2.xlsx');
-    $worksheet = $spreadsheet->getActiveSheet();
-
-    // Get the highest row and column numbers referenced in the worksheet
-    $highestRow = $worksheet->getHighestDataRow(); // e.g. 10
-    $highestColumn = $worksheet->getHighestDataColumn(); // e.g 'F'
-    $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($highestColumn); 
-
-    echo "highest row: $highestRow\n";
+echo "$_SERVER['DOCUMENT_ROOT]";
