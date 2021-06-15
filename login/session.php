7b19d70279214eefe17130399b19dff2ba4f1334
govindparulekar <devgovind511@gmail.com>
2021-06-15 15:50:53 +0530

    yo

--- login/session.php
+++ login/session.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
+$doc_root = $_SERVER['DOCUMENT_ROOT'];
 //echo 'helo';
 if (!isset($_SESSION['logged_in']) && !$_SESSION['logged_in'] === true) {
-  header("location: $_SERVER['DOCUMENT_ROOT']/placement-portal/admin/index.php");
+  header("location: ../admin/index.php");
   exit;
 }
 
